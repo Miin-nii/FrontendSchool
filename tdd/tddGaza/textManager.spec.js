7673537c16325a2c88e96dd1fc7a5e4d6e819b85
Miin-nii <alsdl3344@gmail.com>
2022-06-28 16:24:41 +0900

    6/28 수업내용

---
+++ tdd/tddGaza/textManager.spec.js
@@ -0,0 +1,24 @@
+describe('텍스트 관리자입니다.', () => {
+    let textManager;
+
+    // textManager = new TextManager();
+
+    // it 함수 호출 직전에 실행
+    beforeEach(() => {
+        textManager = new TextManager();
+    });
+
+    it('텍스트 값을 전달합니다.', () => {
+        const initValue = textManager.getValue();
+        expect(textManager.getValue()).toBe(initValue);
+    })
+
+    it('텍스트 값을 수정합니다.', () => {
+
+        const newValue = {data : 'Hello Zebras'};
+
+        textManager.setValue(newValue);
+
+        expect(textManager.getValue()).toBe(newValue.data);
+    })
+})
